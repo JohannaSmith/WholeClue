e9a6b6f2a6bf1e301d2733f32f749372728bfc7f
Charles Tandy <ctandy@mymail.mines.edu>
2014-02-20 15:31:31 -0700

    Modified calcAdjacencies method

--- src/intBoard/IntBoard.java
+++ src/intBoard/IntBoard.java
@@ -1,21 +1,32 @@
 package intBoard;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class IntBoard {
 	// Instance Vars
-	
+	private Map<Integer, ArrayList<Integer>> adjMtx;
+	private boolean[] visited;
+	private static final int COLUMNS = 4;
+	private static final int ROWS = 4;
 	
 	// Constructor
 	public IntBoard() {
 		super();
-		
 	}
 	// Methods
 	public void calcAdjacencies() {
-		// Stub
+		// Init Map
+		adjMtx = new HashMap<Integer, ArrayList<Integer>>();
+		// Calc Adjacencies
+		for(int i = 1; i < COLUMNS; i++) { // iterate columns
+			for(int j = 1; j < ROWS; j++) { // iterate rows
+				
+			}
+		}
 	}
 	public void startTargets(int cell, int steps) { // Calculate
 		// Stub
@@ -29,7 +40,6 @@ public class IntBoard {
 		return new ArrayList<Integer>();
 	}
 	public int calcIndex(int r, int c) {
-		// Stub
-		return -1;
+		return r * 4 + c;
 	}
 }
