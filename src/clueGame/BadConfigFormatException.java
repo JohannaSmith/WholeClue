aa6e5e52d3dd73439cd99f29b6c1751deef1b6c8
unknown <jwakefie@CT152-11.adit.mines.edu>
2014-02-20 17:52:45 -0700

    Board stub and exception class added

--- src/clueGame/BadConfigFormatException.java
+++ src/clueGame/BadConfigFormatException.java
@@ -1,5 +1,61 @@
 package clueGame;
 
-public class BadConfigFormatException {
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+public class BadConfigFormatException extends Exception {
+	// Vars
+	private String message;
+	private static final String COLUMNCOUNTMESSAGE = "Column count mismatch.";
+	private static final String ROWCOUNTMESSAGE = "Row count mismatch.";
+	private static final String BADCELLMESSAGE = "Bad cell symbol.";
+	private static final String BADLEGENDMESSAGE = "Legend line does not consist of key/value pair.";
+	private static final String UNKNOWNMESSAGE = "Unknown error in configuration files.";
+	private static final String LOGFILE = "logfile.txt";
+	// Constructors
+	public BadConfigFormatException(char errorcode) {
+		super();
+		switch(errorcode) {
+		case 'c':
+			message = COLUMNCOUNTMESSAGE;
+			break;
+		case 'r':
+			message = ROWCOUNTMESSAGE;
+			break;
+		case 's':
+			message = BADCELLMESSAGE;
+			break;
+		case 'l':
+			message = BADLEGENDMESSAGE;
+			break;
+		}
+		writeLog(LOGFILE);
+	}
+	public BadConfigFormatException() {
+		super();
+		message = UNKNOWNMESSAGE;
+		writeLog(LOGFILE);
+	}
+	private void writeLog(String fname) {
+		try {
+			PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(new File(fname), true)));
+			out.println(toString());
+			out.close();
+		}
+		catch(IOException e) {
+			System.out.println(String.format("Could not write to \"%s\".", fname));
+		}
+	}
+	// ToString Method
+	public String toString() {
+		return message;
+	}
+	// GetMessage Should use The New Message as Well
+	@Override
+	public String getMessage() {
+		return toString();
+	}
 }
--- src/clueGame/Board.java
+++ src/clueGame/Board.java
@@ -1,5 +1,47 @@
 package clueGame;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Map;
+
 public class Board {
-	
+	// Instance Vars
+	ArrayList<BoardCell> cells;
+	Map<Character,String> rooms;
+	int numRows;
+	int numColumns;
+	// Filenames
+	private static final String LEGENDFILE = "legend.csv";
+	private static final String BOARDLAYOUTFILE = "boardlayout.csv";
+	// Constructor
+	public Board() {
+		super();
+		// Stub
+	}
+	// Load Board
+	public void loadConfigFiles() {
+		loadLegend(new File(LEGENDFILE));
+		loadBoard(new File(BOARDLAYOUTFILE));
+	}
+	private void loadLegend(File f) {
+		// Stub
+	}
+	private void loadBoard(File f) {
+		// Stub
+	}
+	// Calculate Index
+	public int calcIndex(int r, int c) {
+		// Stub
+		return -1;
+	}
+	// Getters
+	public BoardCell getCell(int i) {
+		return cells.get(i);
+	}
+	public BoardCell getCell(int r, int c) {
+		return getCell(calcIndex(r, c));
+	}
+	public String getRoom(char c) {
+		return rooms.get(c);
+	}
 }
