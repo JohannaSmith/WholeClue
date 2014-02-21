f26f1007644e487a53d7157337d4897366c0ec46
Charles Tandy <ctandy@mymail.mines.edu>
2014-02-20 18:17:13 -0700

    updated testBoardDimensions

--- src/testing/BoardTests.java
+++ src/testing/BoardTests.java
@@ -2,6 +2,7 @@ package testing;
 
 import static org.junit.Assert.*;
 
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -9,6 +10,8 @@ import clueGame.Board;
 
 public class BoardTests {
 	private static Board board;
+	private final int ROWS = 25;
+	private final int COLUMNS = 25;
 	
 	@BeforeClass
 	public static void setUp() {
@@ -16,8 +19,9 @@ public class BoardTests {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testBoardDimensions() {
+		Assert.assertEquals(ROWS, board.getNumRows());
+		Assert.assertEquals(COLUMNS, board.getNumColumns());
 	}
 
 }
