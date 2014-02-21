0bf8bbfdb5292f2fca11e586eff6050af9e8957e
Charles Tandy <ctandy@mymail.mines.edu>
2014-02-20 17:54:27 -0700

    added test class

---
+++ src/testing/BoardTests.java
@@ -0,0 +1,23 @@
+package testing;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import clueGame.Board;
+
+public class BoardTests {
+	private static Board board;
+	
+	@BeforeClass
+	public static void setUp() {
+		//board = new Board("boardLayout.csv", "legend.txt");
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
