d5babd533677790c6698c34c6a31833ab77f445f
unknown <jwakefie@CT156-21.adit.mines.edu>
2014-02-23 22:21:52 -0700

    givenfilesadded

--- src/clueTests/BoardTests.java
+++ src/clueTests/BoardTests.java
@@ -1,4 +1,4 @@
-package testing;
+package clueTests;
 
 import static org.junit.Assert.*;
 
--- src/clueTests/IntBoardTests.java
+++ src/clueTests/IntBoardTests.java
@@ -1,4 +1,4 @@
-package testing;
+package clueTests;
 
 import java.util.ArrayList;
 import java.util.Set;
