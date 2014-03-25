78b28eef3b2eb0744d17915220a07326678893c9
Krista Horn <khorn@mines.edu>
2014-03-25 10:40:03 -0600

    Pushing start of JPanel for detective notes to be able to work in unison.

--- src/clueGame/DetectiveNotesGUI.java
+++ src/clueGame/DetectiveNotesGUI.java
@@ -1,5 +1,30 @@
 package clueGame;
 
-public class DetectiveNotesGUI {
+import java.awt.GridLayout;
 
+import javax.swing.JCheckBox;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+
+public class DetectiveNotesGUI extends JDialog {
+	private JDialog guesses = new JDialog();
+	private JPanel peopleList = new JPanel();
+	private JCheckBox scarlet, green, plum, mustard, white, peacock;
+	
+	public DetectiveNotesGUI() {
+		setSize(600, 300);
+		setLayout(new GridLayout(3, 2));
+	}
+	
+	public void setupPanels() { // read names from file
+		scarlet = new JCheckBox("Miss Scarlet");
+		green = new JCheckBox("Mr. Green");
+		plum = new JCheckBox("Professor Plum");
+		mustard = new JCheckBox("Colonel Mustard");
+		white = new JCheckBox("Mrs. White");
+		peacock = new JCheckBox("Mrs. Peacock");
+	}
+	
+	
+	
 }
